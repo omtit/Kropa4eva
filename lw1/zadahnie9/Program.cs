61f909a5696a2aaeb9e2dbef1216db9f52cbc4bd
ynwa13th <gerrard013>
2017-09-29 20:32:50 +0700

    lw1 + zadanie 9

---
+++ lw1/zadahnie9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadahnie9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String ID;
+            ID = Console.ReadLine();
+            Console.WriteLine("SELECT first_name, last_name, group FROM students WHERE id = '" + ID +"';");
+
+        }
+    }
+}
