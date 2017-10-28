9804602b98442a3274762b61006d8b47948fef5e
akryukov <akryukov992@gmail.com>
2017-10-28 10:53:32 +0600

    Материал для лабораторной №12

---
+++ lw12/Methods/Lab01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Lab01
+    {
+        public static void task1860(String a)
+        {
+             Console.WriteLine("Мы стремимся к " + a);
+        }
+
+        public static void task7472(String a, String b)
+        {
+            Console.WriteLine(b + " " + a );
+        }
+    }
+}
---
+++ lw12/Methods/Lab02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Lab02
+    {
+        public static double task4411(double x)
+        {
+            double r;
+            r = x + 7;
+            return r;
+        }
+
+        public static double gradToRad(double a)
+        {
+			
+        }
+    }
+}
