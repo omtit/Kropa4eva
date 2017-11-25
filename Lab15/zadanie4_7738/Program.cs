c0411aac0ab6e28090de551bcc22d494f87d15f2
akryukov <akryukov992@gmail.com>
2017-11-25 12:51:46 +0600

    15

---
+++ Lab15/zadanie4_7738/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace zadanie4_7738
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                String r = "task7738\\test" + i + ".txt";
+                try
+                {
+                    using (StreamReader str = new StreamReader(r))
+                    {
+                        Dictionary<String, int> dct = new Dictionary<String, int>();
+                        while (!str.EndOfStream)
+                        {
+                            String a = str.ReadLine();
+                            if (dct.ContainsKey(a))
+                            {
+                                dct[a]++;
+                            }
+                            else
+                            {
+                                dct.Add(a, 1);
+                            }
+                        }
+
+                        foreach (var a in dct)
+                        {
+                            Console.WriteLine(a.Key + " " + a.Value);
+                        }
+                    }
+                    Console.WriteLine(  );
+                    Console.WriteLine();
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine("Net faila");
+                }
+            }
+
+        }
+    }
+}
---
+++ lw12/Lab15/5087.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 2, 3, 4, 1, 2 };
+           
+            List<int> L = a.ToList();
+            HashSet<int> hashset = new HashSet<int>();
+            foreach (var item in a)
+            {
+                hashset.Add(item);
+            }
+
+            Console.WriteLine(String.Join(";", L));
+            Console.WriteLine(String.Join(";",hashset));
+
+            int[] b = new int[] { 1, 1, 3, 3, 2, 2 };
+            List<int> L1 = b.ToList();
+            HashSet<int> hashset1 = new HashSet<int>();
+            foreach (var item in b)
+            {
+                hashset1.Add(item);
+            }
+
+            Console.WriteLine(String.Join(";", L1));
+            Console.WriteLine(String.Join(";", hashset1));
+
+        }
+    }
+}
